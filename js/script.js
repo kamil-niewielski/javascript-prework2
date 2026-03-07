b2f98267b59da15284c4d81e2bfd5ec9fcdb300a
Kamil Niewielski <nevel1906.kn@gmail.com>
2026-03-07 12:30:55 +0000

    poprawilem zmienne i stale script.js

--- js/script.js
+++ js/script.js
@@ -8,22 +8,19 @@ function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
 
-var argMoveId, argPlayerMove, argComputerMove, argButtonName, computerMove, buttonRock,
-buttonPaper, buttonScissors ;
-
-buttonRock = document.getElementById('button-rock');
-buttonPaper = document.getElementById('button-paper');
-buttonScissors = document.getElementById('button-scissors');
+const buttonRock = document.getElementById('button-rock');
+const buttonPaper = document.getElementById('button-paper');
+const buttonScissors = document.getElementById('button-scissors');
 
 function buttonClicked(argButtonName){
     clearMessages();
     console.log(argButtonName + ' został kliknięty');
 
-    let playerMove = argButtonName;
+    const playerMove = argButtonName;
     console.log('ruch gracza to: ' + playerMove);
     let randomNumber = Math.floor(Math.random() * 3 + 1);
     console.log('wylosowana liczba to: ' + randomNumber);
-    let computerMove = getMoveName(randomNumber);
+    const computerMove = getMoveName(randomNumber);
     console.log('ruch komputera to: ' + computerMove);
     displayResult(playerMove, computerMove);
 }        
